e786f4bdbfaed1c1ea57a83342225087a3c4eaec
Prlfma <kordan.pavlo@gmail.com>
2025-07-30 15:42:02 +0300

    fixed rois extraction

--- Temporary_rois_extractor.py
+++ Temporary_rois_extractor.py
@@ -1,24 +1,20 @@
 import os
 import cv2
 import json
-'''
-    Цей файл вирізає roi для кожного виду помилок які ми детектим.
-    Типи та розміри roi знаходяться в rois.json. Скоріш за все це тимчасовий підхід, але він потрібний
-    для того щоб розділити розробку детекторів та механізм пошуку ROI.
-'''
-
-
 
 # Шляхи
 input_dir = "jpg_discovery"
 output_dir = "rois"
 roi_json_file = "rois.json"
 
-# Завантаження спільного списку ROI
+# Завантаження списку ROI
 with open(roi_json_file, "r") as f:
     roi_data = json.load(f)
+    units = roi_data.get("units", "absolute").lower()  # За замовчуванням absolute
     rois = roi_data.get("rois", [])
-
+if units == "relative":
+    print("Realtive units are used, images can be different sizes")
+              
 # Створення головної папки для збереження ROI
 os.makedirs(output_dir, exist_ok=True)
 
@@ -33,12 +29,27 @@ for filename in os.listdir(input_dir):
             print(f"[!] Неможливо прочитати зображення {img_path}, пропущено.")
             continue
 
+        img_height, img_width = img.shape[:2]
+
         for idx, roi in enumerate(rois):
             roi_type = roi.get("type", "unknown")
+
             x = roi.get("x", 0)
             y = roi.get("y", 0)
             w = roi.get("w", 0)
             h = roi.get("h", 0)
+
+            # Перевірка одиниць виміру
+            if units == "relative":
+                x = int(x * img_width)
+                y = int(y * img_height)
+                w = int(w * img_width)
+                h = int(h * img_height)
+            else:
+                x = int(x)
+                y = int(y)
+                w = int(w)
+                h = int(h)
 
             # Вирізання області
             crop = img[y:y+h, x:x+w]
