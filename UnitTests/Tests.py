c733447e1cf817d422677e69f73f50627e7ae29a
Prlfma <kordan.pavlo@gmail.com>
2025-10-20 16:03:37 +0300

    UnitTests

--- UnitTests/Tests.py
+++ UnitTests/Tests.py
@@ -1,17 +1,19 @@
 import unittest
 import pandas
-from TapeDeviationDetector import TapeDeviationDetector, tape_deviation_detector_factory
+from detectors.tape_deviation_detector import TapeDeviationDetector
 from ultralytics import YOLO
 import os
-dataframe_path = "labels_parsed.csv"
+dataframe_path = "UnitTests/labels_parsed.csv"
 dataset_path = "yolo11dataset_grouped"
-model_path = "Models/best.pt"
+model_path = "models/tape_detector.pt"
+positions_json = "configs/positions.json"
 
 class TestTapeDeviationDetector(unittest.TestCase):
     def test_detector(self):
         df = pandas.read_csv(dataframe_path)
         df["GROI"] = (df["Зона"]-2)*-5+df['ROI'] - 2*(df['class']-1)
         model = YOLO(model_path)
+        count = 0
         for root, dirs, files in os.walk(dataset_path):
             for file in files:
                 
@@ -36,22 +38,19 @@ class TestTapeDeviationDetector(unittest.TestCase):
                                 
                             x, y, w, h = boxes.xywhn[idx].cpu().numpy()
 
-                            tdd = tape_deviation_detector_factory(groi-1)
-                            result = tdd.is_tape_correct(x,w)
+                            tdd = TapeDeviationDetector(positions_json)
+                            result = tdd.is_tape_correct(groi-1,x,w)
+                            count+=1
                             if test_case == 6 and groi == 6:
                                 self.assertEqual(result,-1)
                             elif test_case == 5 and groi == 7:
                                 self.assertEqual(result,1)
                             else:
                                 self.assertEqual(result,0)
+        print(count)
 
 
 
 if __name__ == "__main__":
     unittest.main()
-    # model = YOLO(model_path)
-    # result = model("yolo11dataset_grouped/valid/images/Test_Case_4_Z2_E-1_3_ROI_001_png.rf.d1aa9fd804a511c916c858a252baa259.jpg")
-    # print(result[0].boxes)
-    # print(len(result[0].boxes))
-    # x, y, w, h = result[0].boxes.xywhn[0].cpu().numpy()
-    # print(x, y, w, h)
+    
